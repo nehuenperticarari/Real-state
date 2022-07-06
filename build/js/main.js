9096c049bda4b356142951496143e2b90c04a62a
Nehuen Perticarari <nehuenperticarari@gmail.com>
2022-07-06 14:19:05 -0300

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,2 @@
+const images=document.querySelectorAll(".estate__img");window.addEventListener("scroll",()=>{const e=this.scrollY/-20;images.forEach(o=>{o.style.backgroundPositionY=e+"px"})});
+//# sourceMappingURL=main.js.map
